95be1b8d533895f94ea78fad8458317c3f261efc
Youssef Topaji <youssefftopaji@gmail.com>
2024-05-28 18:12:51 +0200

    update vite configuration

--- mealSharingApp/vite.config.js
+++ mealSharingApp/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 
-// https://vitejs.dev/config/
+dotenv.config();
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    sourcemap: true, // Enable source maps
+  },
+});
